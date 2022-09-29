b5aa5a4ca940029090a4ab1b5c1b635cfe098eec
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-09-28 23:33:50 -0400

    Lab 08 Backend

--- server.js
+++ server.js
@@ -1,45 +1,74 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const data = require("./data/weather.json");
+const axios = require("axios");
 
 const app = express();
 
 app.use(cors());
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
+// Constructor functions =======================================
+class Forecast {
+  constructor(item) {
+    this.date = item.valid_date;
+    this.description = `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
+  }
+}
+
+class Movie {
+  constructor(item) {
+    this.title = item.title;
+    this.overview = item.overview;
+    this.average_votes = item.vote_average;
+    this.total_votes = item.vote_count;
+    this.image_url = `https://image.tmdb.org/t/p/w500${item.poster_path}`;
+    this.popularity = item.popularity;
+    this.released_on = item.release_date;
+  }
+}
+
+// Routes Functions =====================================================
+async function getWeather(req, res) {
+  const { lat, lon,} = req.query;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+  try {
+    const weatherResponse = await axios.get(url);
+    const weatherDataArray = weatherResponse.data.data.map((day) => {
+      return new Forecast(day);
+    });
+    res.send(weatherDataArray);
+  } catch (error) {
+    res.status(500).send("Server Error ", error);
+  }
+}
+
+async function getMovies(req, res) {
+  const { query } = req.query;
+  console.log(query);
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`;
+  try {
+    const movieResponse = await axios.get(url);
+    const movieDataArray = movieResponse.data.results.map((movie) => {
+      return new Movie(movie);
+    });
+    res.send(movieDataArray);
+  } catch (error) {
+    res.status(500).send("Server Error ", error);
+  }
+}
+
+// Routes  =====================================================
 app.get("/", (req, res) => {
   res.send("Hello from backend");
 });
-
-app.get("/weather", (req, res) => {
-  const { lat, lon, searchWeatherQuery } = req.query;
-
-  const weatherData = data.find((item) => {
-    return item.city_name.toLowerCase() === searchWeatherQuery.toLowerCase();
-  });
-  console.log(req.query);
-  class Forecast {
-    constructor(item) {
-      this.date = item.valid_date;
-      this.description = `Low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`;
-    }
-  }
-
-  if (weatherData) {
-    const weatherArr = weatherData.data.map((item) => {
-      return new Forecast(item);
-    });
-    res.send(weatherArr);
-  } else {
-    res.status(500).send("Sorry, something went wrong");
-  }
-});
+app.get("/weather", getWeather);
+app.get("/movies", getMovies);
 
 app.get("*", (req, res) => {
   res.status(404).send("404 Not Found");
